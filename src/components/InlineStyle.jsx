f1b982d7bc64b6d48d8c638268399ac69db4b2dd
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-06 15:46:04 +0900

    スタイルをあてる

--- src/components/InlineStyle.jsx
+++ src/components/InlineStyle.jsx
@@ -1,8 +1,32 @@
 export const InlineStyle = () => {
+  const continerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+
+  const titleStyle = {
+    margin: 0,
+    color: "#3b84a8"
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+
   return (
-    <div>
-      <p>- Inline Styles -</p>
-      <button>FIGHT!</button>
-    </div>
+    <>
+      <div style={continerStyle}>
+        <p style={titleStyle}>- Inline Styles -</p>
+        <button style={buttonStyle}>FIGHT!</button>
+      </div>
+    </>
   );
 };
