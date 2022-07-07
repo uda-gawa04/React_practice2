0ffbbb924af3d6a06419b5bb1d2e522428706b79
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-07 10:42:49 +0900

    Styled_JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/Styled";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
